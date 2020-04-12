63668c4d5ac77bb3b86f3f903f71320f9d5d4ec3
Akshat Jain <akjn11@gmail.com>
2020-04-13 01:27:19 +0530

    Modularised the code into classes using SOLID principles

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import json
-with open('input.json', 'r') as myfile:
+with open('input2.json', 'r') as myfile:
     x=myfile.read()
 
 data = json.loads(x)
@@ -8,124 +8,135 @@ outputfile = open(outputFileName, 'w')
 
 tab=0
 
-def printTabs(k):
-	for i in range(0,k):
-		print('\t', file=outputfile, end="")
+class PragmaDirective():
+	def __init__(self, node):
+		self.node = node
 
-def decodeAssignmentExpression(node):
-	global tab
-	printTabs(tab)
-	expression = node['expression']
-	print(expression['leftHandSide']['name'] + ' = ' + expression['rightHandSide']['value'] + ';', file=outputfile)
+	def decode(self):
+		print('pragma ' + str(self.node['literals'][0]) + ' ' + str(self.node['literals'][1]) + str(self.node['literals'][2]) + ';', file=outputfile)
 
-def decodeExpressionStatement(node):
-	if(node['expression']['nodeType']=='Assignment'):
-		decodeAssignmentExpression(node)
+class ContractDefinition():
+	def __init__(self, node):
+		self.node = node
 
-def decodeReturnStatement(node):
-	global tab
-	printTabs(tab)
-	returnStatementString = ""
-	if(node['expression']['nodeType']=='Identifier'):
-		returnStatementString += 'return ' + str(node['expression']['name']) + ', '
-		returnStatementString = returnStatementString[:-2]
-	elif(node['expression']['nodeType']=='TupleExpression'):
-		for component in node['expression']['components']:
-			returnStatementString += str(component['name']) + ', '
-		returnStatementString = returnStatementString[:-2]
-		returnStatementString = 'return (' + returnStatementString + ')'
-	print(returnStatementString + ';', file=outputfile)
+	def decode(self):
+		print('contract ' + str(self.node['name']) + '{', file=outputfile)
+		global tab
+		tab += 1
+		for i in self.node['nodes']:
+			nodeType = i['nodeType']
+			if(nodeType=='VariableDeclaration'):
+				self.decodeVariableDeclaration(i)
+			elif(nodeType=='FunctionDefinition'):
+				self.decodeFunctionDefinition(i)
+		tab -= 1
+		print('}', file=outputfile)
 
-def decodeConstructor(node):
-	global tab
-	print('constructor() ' + node['visibility'] + '{', file=outputfile)
-	tab += 1
-	statements = node['body']['statements']
-	# print(statements, file=outputfile)
-	for statement in statements:
-		if(statement['nodeType']=='ExpressionStatement'):
-			decodeExpressionStatement(statement)
-	tab -= 1
-	printTabs(tab)
-	print('}', file=outputfile)
+	def decodeVariableDeclaration(self, node):
+		global tab
+		self.printTabs(tab)
+		print(node['typeName']['name'] + ' ' + node['name'], file=outputfile, end="")
+		value = node['value']
+		if(value is not None):
+			print(' = ' + str(value), file=outputfile, end="")
+		print(';', file=outputfile)
 
-def decodeFunction(node):
-	# State mutability: pure, view, nonpayable, or payable
-	
-	global tab
+	def decodeFunctionDefinition(self, node):
+		global tab
+		self.printTabs(tab)
+		if(node['kind']=='constructor'):
+			self.decodeConstructor(node)
+		elif(node['kind']=='function'):
+			self.decodeFunction(node)
 
-	parameters = node['parameters']
-	parametersString = ""
+	def decodeConstructor(self, node):
+		global tab
+		print('constructor() ' + node['visibility'] + '{', file=outputfile)
+		tab += 1
+		statements = node['body']['statements']
+		# print(statements, file=outputfile)
+		for statement in statements:
+			if(statement['nodeType']=='ExpressionStatement'):
+				self.decodeExpressionStatement(statement)
+		tab -= 1
+		self.printTabs(tab)
+		print('}', file=outputfile)
 
-	returnParameters = node['returnParameters']
-	returnParametersString = ""
-	
-	for parameter in parameters['parameters']:
-		parametersString += str(parameter['typeName']['name']) + ', '
+	def decodeFunction(self, node):
+		# State mutability: pure, view, nonpayable, or payable
 
-	for returnParameter in returnParameters['parameters']:
-		returnParametersString += str(returnParameter['typeName']['name']) + ', '
+		global tab
 
-	parametersString = parametersString[:-2]
+		parameters = node['parameters']
+		parametersString = ""
 
-	returnParametersString = returnParametersString[:-2]
-	returnParametersString = ' returns(' + returnParametersString + ')'
+		returnParameters = node['returnParameters']
+		returnParametersString = ""
 
-	if(node['stateMutability']=='nonpayable'):
-		print('function ' + node['name'] + '(' + parametersString + ') ' + node['visibility'] + returnParametersString + '{', file=outputfile)
-	else:
-		print('function ' + node['name'] + '(' + parametersString + ') ' + node['visibility'] + ' ' + node['stateMutability'] +  returnParametersString + '{', file=outputfile)
-	
-	tab += 1
+		for parameter in parameters['parameters']:
+			parametersString += str(parameter['typeName']['name']) + ', '
 
-	statements = node['body']['statements']
-	for statement in statements:
-		if(statement['nodeType']=='ExpressionStatement'):
-			decodeExpressionStatement(statement)
-		elif(statement['nodeType']=='Return'):
-			decodeReturnStatement(statement)
+		for returnParameter in returnParameters['parameters']:
+			returnParametersString += str(returnParameter['typeName']['name']) + ', '
 
-	tab -= 1
-	printTabs(tab)
-	print('}', file=outputfile)
-	pass
+		parametersString = parametersString[:-2]
 
-def decodeVariableDeclaration(node):
-	global tab
-	printTabs(tab)
-	print(node['typeName']['name'] + ' ' + node['name'], file=outputfile, end="")
-	value = node['value']
-	if(value is not None):
-		print(' = ' + str(value), file=outputfile, end="")
-	print(';', file=outputfile)
+		returnParametersString = returnParametersString[:-2]
+		returnParametersString = ' returns(' + returnParametersString + ')'
 
-def decodeFunctionDefinition(node):
-	global tab
-	printTabs(tab)
-	if(node['kind']=='constructor'):
-		decodeConstructor(node)
-	elif(node['kind']=='function'):
-		decodeFunction(node)
+		if(node['stateMutability']=='nonpayable'):
+			print('function ' + node['name'] + '(' + parametersString + ') ' + node['visibility'] + returnParametersString + '{', file=outputfile)
+		else:
+			print('function ' + node['name'] + '(' + parametersString + ') ' + node['visibility'] + ' ' + node['stateMutability'] +  returnParametersString + '{', file=outputfile)
 
-def decodePragmaDirective(node):
-	print('pragma ' + str(node['literals'][0]) + ' ' + str(node['literals'][1]) + str(node['literals'][2]) + ';', file=outputfile)
+		tab += 1
 
-def decodeContractDefinition(node):
-	print('contract ' + str(node['name']) + '{', file=outputfile)
-	global tab
-	tab += 1
-	for i in node['nodes']:
-		nodeType = i['nodeType']
-		if(nodeType=='VariableDeclaration'):
-			decodeVariableDeclaration(i)
-		elif(nodeType=='FunctionDefinition'):
-			decodeFunctionDefinition(i)
-	tab -= 1
-	print('}', file=outputfile)
+		statements = node['body']['statements']
+		for statement in statements:
+			if(statement['nodeType']=='ExpressionStatement'):
+				self.decodeExpressionStatement(statement)
+			elif(statement['nodeType']=='Return'):
+				self.decodeReturnStatement(statement)
+
+		tab -= 1
+		self.printTabs(tab)
+		print('}', file=outputfile)
+		pass
+
+	def decodeAssignmentExpression(self, node):
+		global tab
+		self.printTabs(tab)
+		expression = node['expression']
+		print(expression['leftHandSide']['name'] + ' = ' + expression['rightHandSide']['value'] + ';', file=outputfile)
+
+	def decodeExpressionStatement(self, node):
+		if(node['expression']['nodeType']=='Assignment'):
+			self.decodeAssignmentExpression(node)
+
+	def decodeReturnStatement(self, node):
+		global tab
+		self.printTabs(tab)
+		returnStatementString = ""
+		if(node['expression']['nodeType']=='Identifier'):
+			returnStatementString += 'return ' + str(node['expression']['name']) + ', '
+			returnStatementString = returnStatementString[:-2]
+		elif(node['expression']['nodeType']=='TupleExpression'):
+			for component in node['expression']['components']:
+				returnStatementString += str(component['name']) + ', '
+			returnStatementString = returnStatementString[:-2]
+			returnStatementString = 'return (' + returnStatementString + ')'
+		print(returnStatementString + ';', file=outputfile)
+
+	def printTabs(self, k):
+		for i in range(0,k):
+			print('\t', file=outputfile, end="")
+
 
 for i in data['nodes']:
 	nodeType = i['nodeType']
 	if(nodeType=='PragmaDirective'):
-		decodePragmaDirective(i)
+		x = PragmaDirective(i)
+		x.decode()
 	elif(nodeType=='ContractDefinition'):
-		decodeContractDefinition(i)
+		x = ContractDefinition(i)
+		x.decode()
